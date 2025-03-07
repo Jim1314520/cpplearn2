2b4eef9a7dd7b247c088cc083a71e3fc34d7d0c7
Jim1314520 <2942566932@qq.com>
2025-03-07 09:26:07 +0800

    2

--- wrong2/b2073.cpp
+++ wrong2/b2073.cpp
@@ -3,8 +3,13 @@ using namespace std;
 #include <vector>
 #include <algorithm>
 int main(){
-  int a, b, n;
+  int a, b, n, res;
   cin >> a >> b >> n;
-  int c;
-  int v;
+  a %= b; // 20 / 6 = 3 .... 2   2 * 10 = 20 第一位小数 20 / 6 = 3...2
+  while(n--){
+    a *= 10;
+    res = a / b;
+    a %= b;
+  }
+  cout << res << endl;
 }
