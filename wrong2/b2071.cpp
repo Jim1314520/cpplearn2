1e2b6d31e0664318044a171f97c06416a83de28c
Jim1314520 <2942566932@qq.com>
2025-03-07 09:28:23 +0800

    22

---
+++ wrong2/b2071.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+int main(){
+ int a, b, c;
+ cin >> a >> b >> c;
+ for(int i = 2; ; i++){
+  if(a % i == b % i && b % i == c % i){
+    cout << i << endl;
+    break;
+  }
+ }
+}
